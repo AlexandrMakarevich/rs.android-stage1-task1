82acd670623524ab18e5727b095194a59546e2aa
Alex Makarevich <lemas096@gmail.com>
2020-03-27 11:25:23 +0300

    feat: complited tasks 1,2,3,4

--- src/main/kotlin/subtask4/StringParser.kt
+++ src/main/kotlin/subtask4/StringParser.kt
@@ -1,9 +1,54 @@
 package subtask4
 
+
 class StringParser {
+    var stringBuilder: StringBuilder = java.lang.StringBuilder()
+    var outputStringArray = emptyArray<String>()
+
 
     // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+
+
+        for (index in inputString.indices) {
+
+            if (inputString[index] == '(') {
+                findCornerBraces(inputString, index, '(', ')')
+            }
+
+            if (inputString[index] == '[') {
+                findCornerBraces(inputString, index, '[', ']')
+            }
+
+            if (inputString[index] == '<') {
+                findCornerBraces(inputString, index, '<', '>')
+            }
+        }
+
+        outputStringArray.forEach { item -> println(item) }
+
+        return outputStringArray
+    }
+
+    fun findCornerBraces(inputString: String, index: Int, charIn: Char, charOut: Char) {
+        var i = 0
+        for (index1 in index + 1 until inputString.length) {
+
+            if (inputString[index1] == charIn) i += 1
+            if (inputString[index1] != charOut) {
+                stringBuilder.append(inputString[index1])
+            }
+            else {
+                if (i == 0) {
+                    outputStringArray += stringBuilder.toString()
+                    stringBuilder.delete(0, stringBuilder.length)
+                    break
+                } else {
+                    stringBuilder.append(inputString[index1])
+                    if (inputString[index1] == charOut) {i -= 1}
+                }
+            }
+        }
     }
 }
+

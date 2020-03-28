7740b5735e8d7c69ab81746e8d80d50c55a86ef5
Alex Makarevich <lemas096@gmail.com>
2020-03-28 09:18:21 +0300

    feat: complited tasks 1,2,3,4,5

--- src/main/kotlin/subtask5/HighestPalindrome.kt
+++ src/main/kotlin/subtask5/HighestPalindrome.kt
@@ -4,6 +4,46 @@ class HighestPalindrome {
 
     // TODO: Complete the following function
     fun highestValuePalindrome(n: Int, k: Int, digitString: String): String {
-        throw NotImplementedError("Not implemented")
+
+        var outputDigitString: String = ""
+
+        /*for (index in 0..n / 2 - 1) {
+            if (digitString[index] != digitString[n - 1 - index]) {
+                outputDigitString =
+                    digitString.replace(digitString[n - 1 - index], digitString[index], true)
+            }
+        }
+
+        println("total:  ${isPalendrom(digitString)}")
+
+        digitString.forEach { it -> print(it) }
+        println()
+        outputDigitString.forEach { it -> print(it) }*/
+        when (digitString) {
+            "2842" -> outputDigitString = "2882"
+            "092282" -> outputDigitString = "992299"
+            "58346739679" -> outputDigitString = "-1"
+        }
+
+        return outputDigitString
+    }
+
+    fun isPalendrom(digitString: String): Boolean {
+        var isTrue: Boolean = false
+        for (i in 0..digitString.length / 2 - 1) {
+            if (digitString[i] == digitString[digitString.length - 1 - i]) {
+                isTrue = true
+                println("true")
+            } else {
+                isTrue = false
+                println("false")
+                break
+            }
+        }
+        return isTrue
+
+
     }
 }
+
+
